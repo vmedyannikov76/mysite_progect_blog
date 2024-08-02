9ae9dc9f980c0fbb38bafa03046196d4ee5e3e6d
medyannikov_vemz <vmedyannikov@gmail.com>
2024-08-02 11:29:05 +0300

    представления на основе классов

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator
+from django.views.generic import ListView
 
 
-def post_list(request): # все сообщения
+class PostListView(ListView):
+    '''Альтернативное представление списка постов'''
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_list(request):  # все сообщения
+    # Постраничная разбивка
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
     posts = Post.published.all()
     return render(request,
                   'blog/post/list.html',
